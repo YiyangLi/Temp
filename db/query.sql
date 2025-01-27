17c1e10dfe790aaabac0f4f8c5b6d407b4858256
YiyangLi <yiyangli2010@gmail.com>
2025-01-27 14:33:29 -0800

    move query.sql

--- db/query.sql
+++
@@ -1,8 +0,0 @@
-SELECT d.date,
-SUM(s.weekly_sales) AS total_sales,
-AVG(s.weekly_sales) AS average_weekly_sales
-FROM sales s 
-INNER JOIN dates d  on s.date_id = d.id
-WHERE d.date >= '2022-11-01' ANd d.date <= '2022-11-30'
-GROUP BY d.date
-ORDER BY d.date
